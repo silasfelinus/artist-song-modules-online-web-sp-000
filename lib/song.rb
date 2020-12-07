44daa15abe6ee60a18e1a903beb0fe234bfeee04
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-07 21:10:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,13 +15,6 @@ class Song
     @@songs
   end
 
-#  def self.reset_all
-#    self.all.clear
-#  end
-
-#  def self.count
-#    self.all.count
-#  end
 
   def artist=(artist)
     @artist = artist
